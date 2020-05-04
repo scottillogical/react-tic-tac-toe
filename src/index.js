4250e5c14407edc5c84262d3b2608d98f4ed26a7
Scott Schulthess <sschulthess@zipcar.com>
2020-05-04 10:39:55 -0400

    remove comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//class Square extends React.Component {
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
